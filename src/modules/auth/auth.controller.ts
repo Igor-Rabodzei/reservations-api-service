bff2100d2b29cdd5f33990cd7574c9bb21a13a58
Igor-Rabodzei <igor.rabodzei@gmail.com>
2024-02-17 23:01:34 +0200

    implement user module

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,18 +1,29 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  BadRequestException,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 import { AuthService } from './auth.service';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('sign-up')
-  async signUp() {
-    return true;
+  async signUp(@Body() signUpDto: SignInDto) {
+    const data = await this.authService.signUp(signUpDto);
+    if (data.error) throw new BadRequestException(data.error);
+    else return data;
   }
 
   @Post('sign-in')
-  async signIn() {
-    return true;
+  async signIn(@Body() signInDto: SignInDto) {
+    const data = await this.authService.signIn(signInDto);
+    if (data.error) throw new UnauthorizedException(data.error);
+    else return data;
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { User, UserDocument } from 'src/schemas/user.schema';
+import { ICreateUser } from 'src/common/interfaces/create.user';
 
 @Injectable()
 export class UserService {
-  createUser() {
-    return {};
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<UserDocument>,
+  ) {}
+
+  async createUser(params: ICreateUser): Promise<UserDocument> {
+    const user = new this.userModel({
+      name: params.name,
+      password: params.password,
+    });
+
+    await user.save();
+    return user;
+  }
+
+  async findUser(filters: any): Promise<UserDocument> {
+    return await this.userModel.findOne(filters);
   }
 }
