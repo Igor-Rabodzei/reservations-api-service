2d0caf01d76fa38c859034d88521f7273d5b6b8a
Igor-Rabodzei <igor.rabodzei@gmail.com>
2024-02-17 22:59:36 +0200

    add migration amenities

---
+++ migrations/20240215163338-amenities.js
@@ -0,0 +1,22 @@
+module.exports = {
+  async up(db) {
+    return db
+      .collection('amenity')
+      .insertMany([
+        { name: 'Swimming Pool' },
+        { name: 'Gym' },
+        { name: 'Tennis Court' },
+        { name: 'Spa' },
+        { name: 'Sauna' },
+        { name: 'Fitness Center' },
+        { name: 'Basketball Court' },
+        { name: 'Yoga Studio' },
+        { name: 'Game Room' },
+        { name: 'Cinema' },
+      ]);
+  },
+
+  async down(db) {
+    return db.collection('amenity').deleteMany({});
+  },
+};
